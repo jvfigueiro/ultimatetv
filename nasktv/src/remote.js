0bffdad911f5ec240ee6e99a5970285778de44c1
jvfigueiro <ultx5@outlook.com>
2026-07-29 14:47:57 -0300

    fix: ocultar icone play e arrumar tecla OK android tv

--- nasktv/src/remote.js
+++ nasktv/src/remote.js
@@ -6,54 +6,35 @@ export class RemoteController {
 
   init() {
     window.addEventListener('keydown', (e) => {
-      if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight","Space","PageUp","PageDown","Enter","NumpadEnter"].indexOf(e.code) > -1) {
+      const code = e.code || '';
+      const keyCode = e.keyCode || 0;
+      const key = e.key || '';
+
+      const isEnter = code === 'Enter' || code === 'NumpadEnter' || key === 'Enter' || keyCode === 13 || keyCode === 23 || keyCode === 66;
+      const isUp = code === 'ArrowUp' || code === 'Equal' || code === 'PageUp' || key === 'ArrowUp' || keyCode === 38 || keyCode === 19;
+      const isDown = code === 'ArrowDown' || code === 'Minus' || code === 'PageDown' || key === 'ArrowDown' || keyCode === 40 || keyCode === 20;
+      const isLeft = code === 'ArrowLeft' || key === 'ArrowLeft' || keyCode === 37 || keyCode === 21;
+      const isRight = code === 'ArrowRight' || key === 'ArrowRight' || keyCode === 39 || keyCode === 22;
+      const isBack = code === 'Escape' || code === 'Backspace' || key === 'Escape' || keyCode === 27 || keyCode === 8 || keyCode === 4;
+      const isInfo = code === 'KeyI' || keyCode === 73;
+      const isList = code === 'KeyL' || code === 'KeyC' || keyCode === 76 || keyCode === 67;
+      const isGuide = code === 'KeyG' || code === 'KeyE' || keyCode === 71 || keyCode === 69;
+      const isMenu = code === 'KeyM' || code === 'KeyO' || code === 'ContextMenu' || code === 'F10' || keyCode === 77 || keyCode === 79 || keyCode === 82;
+
+      if (isUp || isDown || isLeft || isRight || isEnter || code === 'Space' || keyCode === 32) {
         e.preventDefault();
       }
 
-      switch (e.code) {
-        case 'ArrowUp':
-        case 'Equal':
-        case 'PageUp':
-          this.callbacks.onUp && this.callbacks.onUp();
-          break;
-        case 'ArrowDown':
-        case 'Minus':
-        case 'PageDown':
-          this.callbacks.onDown && this.callbacks.onDown();
-          break;
-        case 'ArrowLeft':
-          this.callbacks.onLeft && this.callbacks.onLeft();
-          break;
-        case 'ArrowRight':
-          this.callbacks.onRight && this.callbacks.onRight();
-          break;
-        case 'Enter':
-        case 'NumpadEnter':
-        case 'Space':
-          this.callbacks.onEnter && this.callbacks.onEnter();
-          break;
-        case 'KeyI':
-          this.callbacks.onInfo && this.callbacks.onInfo();
-          break;
-        case 'KeyL':
-        case 'KeyC':
-          this.callbacks.onList && this.callbacks.onList();
-          break;
-        case 'KeyG':
-        case 'KeyE':
-          this.callbacks.onGuide && this.callbacks.onGuide();
-          break;
-        case 'KeyM':
-        case 'KeyO':
-        case 'ContextMenu':
-        case 'F10':
-          this.callbacks.onMenu && this.callbacks.onMenu();
-          break;
-        case 'Escape':
-        case 'Backspace':
-          this.callbacks.onBack && this.callbacks.onBack();
-          break;
-      }
+      if (isUp) this.callbacks.onUp && this.callbacks.onUp();
+      else if (isDown) this.callbacks.onDown && this.callbacks.onDown();
+      else if (isLeft) this.callbacks.onLeft && this.callbacks.onLeft();
+      else if (isRight) this.callbacks.onRight && this.callbacks.onRight();
+      else if (isEnter || code === 'Space' || keyCode === 32) this.callbacks.onEnter && this.callbacks.onEnter();
+      else if (isBack) this.callbacks.onBack && this.callbacks.onBack();
+      else if (isInfo) this.callbacks.onInfo && this.callbacks.onInfo();
+      else if (isList) this.callbacks.onList && this.callbacks.onList();
+      else if (isGuide) this.callbacks.onGuide && this.callbacks.onGuide();
+      else if (isMenu) this.callbacks.onMenu && this.callbacks.onMenu();
     }, { capture: true });
   }
 }
