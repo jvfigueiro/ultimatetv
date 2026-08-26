e2f647786636cf265e238921f0c51ce5f358e318
João <ultx5@outlook.com>
2026-08-26 17:06:00 -0300

    build: remove @vitejs/plugin-legacy and modernise build target to esnext

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [
-    legacy({
-      targets: [
-        'Chrome >= 30',
-        'Safari >= 6',
-        'iOS >= 6',
-        'IE >= 11',
-        '> 0.2%',
-        'not dead'
-      ],
-      polyfills: true,
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [],
   build: {
-    target: 'es2015',
+    target: 'esnext',
     minify: 'terser',
     rollupOptions: {
       input: {
